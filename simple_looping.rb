20d1e9bf916a6396fc1d21e43accafd657fdd414
Justin Hamlin <justin.hamlin@gmail.com>
2018-02-16 22:03:32 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -26,7 +26,7 @@ def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = l
   while counter < number_of_times
-  puts phras
+  puts phrase
   counter += 1
 end
   
